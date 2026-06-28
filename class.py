6f531240ec4fd229f7da3fd29761f859311cfaa8
Raheenarwork <raheenarwork>
2026-06-28 20:37:30 +0530

    code01

--- class.py
+++ class.py
@@ -1,10 +1,5 @@
-def inttostr(i):
-    dig = '0123456789'
-    if i==0 :
-        return '0'
-    result = ''
-    while i>0 :
-        result  = dig[i%10]+result
-        i=i//10
-        return result
-    print(inttostr(123))
+i = 50 
+if i % 2 == 0:
+    print ("even" )
+else:
+    print("odd")
