44e08bc4047d6c50b915a4890d34a7bc52c0e8de
Raheenarwork <raheenarwork>
2026-06-28 21:56:53 +0530

    search

--- class.py
+++ class.py
@@ -1,5 +1,15 @@
-i = 50 
-if i % 2 == 0:
-    print ("even" )
-else:
-    print("odd")
+def inttostr(i):
+    dig = '0123456789'
+
+    if i == 0:
+        return '0'
+
+    result = ''
+
+    while i > 0:
+        result = dig[i % 10] + result
+        i = i // 10
+
+    return result
+
+print(inttostr(123))
